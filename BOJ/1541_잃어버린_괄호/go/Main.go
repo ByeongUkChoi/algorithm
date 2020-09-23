07812f16073e2c4a6637fbdb67c2b69832bf6359
ByeongUk Choi <cbw0916@gmail.com>
2020-09-24 01:31:25 +0900

    1541 잃어버린 괄호 (go)

---
+++ BOJ/1541_잃어버린_괄호/go/Main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
+
+	result := 0
+	tmp := 0
+	plusOperator := true
+	for _, s := range input {
+		if i, err := strconv.Atoi(string(s)); err == nil {
+			tmp = tmp * 10 + i
+			continue
+		}
+
+		if plusOperator {
+			result += tmp;
+		} else {
+			result -= tmp;
+		}
+		tmp = 0
+		if plusOperator == true && s == '-' {
+			plusOperator = false
+		}
+	}
+	if plusOperator {
+		result += tmp;
+	} else {
+		result -= tmp;
+	}
+	fmt.Print(result)
+}
+
