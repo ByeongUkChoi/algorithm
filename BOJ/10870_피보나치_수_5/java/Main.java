e24fae4bb446edd079d4d841fbf445ef4fc1b0df
ByeongUk Choi <cbw0916@gmail.com>
2020-11-01 15:26:42 +0900

    10870 피보나치 수 5 (java)

---
+++ BOJ/10870_피보나치_수_5/java/Main.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int result = fibonacci(n);
+        System.out.println(result);
+    }
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
