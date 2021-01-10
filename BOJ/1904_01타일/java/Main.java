e051be82cda17ae07048b80aa6a5d3c9b9a58ec6
ByeongUk Choi <cbw0916@gmail.com>
2021-01-10 09:02:04 +0900

    1904 01타일 (java)

---
+++ BOJ/1904_01타일/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 15746;
+        }
+        System.out.println(dp[n]);
+    }
+}
+
