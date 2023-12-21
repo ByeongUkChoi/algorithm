1335d6b30b69e1176d485c5358d2f9b253fee99d
bu <cbw0916@gmail.com>
2023-12-21 23:43:32 +0900

    217. Contains Duplicate (java)

---
+++ LeetCode/217. Contains Duplicate/java/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            if (numSet.contains(num)) {
+                return true;
+            }
+            numSet.add(num);
+        }
+        return false;
+    }
+}
